ac93f5f4c7db267cd36745a5517965aaa795e18c
KingFaiking <41138080+KingFaiking@users.noreply.github.com>
2018-08-16 14:50:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,23 +8,9 @@ client.on('ready', () => {
 
 
 
-client.on('message', msg =>{
-    let message=msg;
-    if(message.content.startsWith("bc")){
-        var args = message.content.split(' ').slice(1).join(' ');
-    msg.guild.members.forEach(m=>{
-        m.send(args.replace(/[user]/g,m)).catch();
-    if(message.attachments.first()){
-m.sendFile(message.attachments.first().url).catch();
-    }
-    })    ;
-    }
-});
-
-
   client.on('ready', function(){
     var ms = 1000 ;
-    var setGame = [`>b | KingsServerBc `,` >bc | KingsServerBc `];
+    var setGame = [`${prefix}b | KingsServerBc `,` ${prefix}bc | KingsServerBc `];
     var i = -1;
     var j = 0;
     setInterval(function (){
